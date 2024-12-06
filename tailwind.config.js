01d4fa402b884aaca837395a7fd861b8b8ca188d
Abdul Samad <abdulsamad52556@gmail.com>
2024-12-06 19:28:20 +0530

    tailwind_config_edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
